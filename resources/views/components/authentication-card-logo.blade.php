8d99ac453fe21257fc3832104fe59b20fa987d5f
José Quiñonez <Jquinonez@tsc.com.gt>
2023-12-17 15:46:12 -0600

    Change navigation menu

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,5 +1,5 @@
 <a href="/">
     <div class="text-gray-800 font-semibold">
-        <span class="text-blue-500 text-xl">&lt;Kompas&gt;</span> Blog
+        <span class="text-blue-500 text-4xl">&lt;Kompas&gt;</span> Blog
     </div>
 </a>
