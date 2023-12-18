5d3a31d67985e51451c97d1acdc8d170df21b998
José Quiñonez <Jquinonez@tsc.com.gt>
2023-12-18 03:17:57 -0600

    Change Hero Section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,12 @@
 <x-app-layout>
     @section('hero')
         <div class="w-full text-center py-32">
-            <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
-                Welcome to <span class="text-yellow-500">&lt;YELO&gt;</span> <span class="text-gray-900"> News</span>
+            <h1 class="text-2xl py-5 md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
+                Explora tu Camino hacia el Éxito con <span class="text-indigo-500">Kompas</span>
             </h1>
-            <p class="text-gray-500 text-lg mt-1">Best Blog in the universe</p>
-            <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block"
-                href="http://127.0.0.1:8000/blog">Start
-                Reading</a>
+            <p class="text-gray-500 text-lg mt-1">Tu guía para encontrar la universidad perfecta para tu camino.</p>
+            <a wire:navigate class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block"
+                href="{{ route('posts.index') }}">Comienza a Explorar</a>
         </div>
     @endsection
 
