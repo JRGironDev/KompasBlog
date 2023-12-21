fc6163d1e2d76f9b9e4180d9a7e86758cd48cae3
José Quiñonez <Jquinonez@tsc.com.gt>
2023-12-21 04:27:16 -0600

    Add Category Filament Component

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -5,13 +5,18 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\CategoryResource\Pages;
 use App\Filament\Resources\CategoryResource\RelationManagers;
 use App\Models\Category;
+use Faker\Provider\ar_EG\Text;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class CategoryResource extends Resource
 {
@@ -23,7 +28,16 @@ class CategoryResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('title')
+                ->live()
+                ->required()->minLength(3)->maxLength(150)
+                ->afterStateUpdated(function(string $operation, $state, Set $set){
+                    if ($operation === 'edit') return;
+                    $set('slug', Str::slug($state));
+                }),
+                TextInput::make('slug')->required()->minLength(3)->maxLength(150)->unique(ignoreRecord: true),
+                TextInput::make('text_color')->nullable(),
+                TextInput::make('bg_color')->nullable(),
             ]);
     }
 
@@ -31,7 +45,10 @@ class CategoryResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('title')->sortable()->searchable(),
+                TextColumn::make('slug')->sortable()->searchable(),
+                TextColumn::make('text_color')->sortable()->searchable(),
+                TextColumn::make('bg-color')->sortable()->searchable()
             ])
             ->filters([
                 //
