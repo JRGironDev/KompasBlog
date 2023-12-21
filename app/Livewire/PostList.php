c086a755d9b861e696126e316abd3b852af6ac4c
José Quiñonez <Jquinonez@tsc.com.gt>
2023-12-20 19:55:05 -0600

    Add Filament Package

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Post;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\Url;
+use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -14,15 +15,27 @@ class PostList extends Component
 
     #[Url()]
     public $sort = 'desc';
+    public $search = '';
 
     public function setSort($sort)
     {
         $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
+        $this->resetPage();
     }
+
+    #[On('search')]
+    public function updateSearch($search)
+    {
+        $this->search = $search;
+    }
+
     #[Computed()]
     public function posts()
     {
-        return Post::published()->orderBy('published_at', $this->sort)->paginate(3);
+        return Post::published()
+        ->orderBy('published_at', $this->sort)
+        ->where('title', 'like', "%{$this->search}%")
+        ->paginate(3);
     }
     public function render()
     {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,15 @@
             <h1 class="text-2xl py-5 md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
                 Explora tu Camino hacia el Éxito con <span class="text-indigo-500">Kompas</span>
             </h1>
-            <p class="text-gray-500 text-lg mt-1">Tu guía para encontrar la universidad perfecta para tu camino.</p>
-            <a wire:navigate class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block"
+            <p class="text-gray-500 text-lg mt-1">La guía para encontrar la universidad perfecta para tu camino.</p>
+            <a wire:navigate class="px-3 py-2 text-lg text-white bg-indigo-500 rounded mt-5 inline-block"
                 href="{{ route('posts.index') }}">Comienza a Explorar</a>
         </div>
     @endsection
 
     <div class="mb-10 w-full">
         <div class="mb-16">
-            <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Featured Posts</h2>
+            <h2 class="mt-16 mb-5 text-3xl text-indigo-500 font-bold">Publicaciones Recomendadas</h2>
             <div class="w-full">
                 <div class="grid grid-cols-3 gap-10 w-full">
                     @foreach ($featuredPosts as $post)
@@ -22,13 +22,12 @@
                     @endforeach
                 </div>
             </div>
-            <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold"
-                href="http://127.0.0.1:8000/blog">More
-                Posts</a>
+            <a class="mt-10 block text-center text-lg text-indigo-500 font-semibold"
+                href="http://127.0.0.1:8000/blog">Más Publicaciones</a>
         </div>
         <hr>
 
-        <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Latest Posts</h2>
+        <h2 class="mt-16 mb-5 text-3xl text-indigo-500font-bold">Publicaciones Recientes</h2>
         <div class="w-full mb-5">
             <div class="grid grid-cols-3 gap-10 w-full">
                 @foreach ($latestPosts as $post)
@@ -38,7 +37,7 @@
                 @endforeach
             </div>
         </div>
-        <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold" href="http://127.0.0.1:8000/blog">More
-            Posts</a>
+        <a class="mt-10 block text-center text-lg text-indigo-500 font-semibold" href="http://127.0.0.1:8000/blog">Más
+            Publicaciones</a>
     </div>
 </x-app-layout>
